e3b91ad94c8035ccfed209a03165ba57bb02adf7
GouseRabbani123 <67941879+GouseRabbani123@users.noreply.github.com>
2022-02-16 23:28:09 +0530

    Day1

---
+++ 15-2-22.js
@@ -0,0 +1,38 @@
+// JavaScript- it is a interpriter programming language. 
+// interpriter: it converts the human code in to meachine code on the spot.
+
+// javascript is object oriented progaramming.
+
+// javascript is also called as untyped.
+
+// we can ddo multiple functionalites in a programming
+
+
+// // Masai and accio jobs just check once
+
+// Javascript variables are the containers for storing the data. 
+
+// // var a = 10;
+// // var a = "Gouse Rabbani"
+
+// // Thats how we decalre the variables
+
+//addition
+ var a = 10;
+ var b = 20;
+ var c = a + b
+ console.log(c)
+ document.write(c)
+
+
+
+
+
+
+
+
+
+
+
+
+
